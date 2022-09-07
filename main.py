9ba44e6d95a6bc38f166046e655587ce952fa647
MagnataDosPardais <marcobee.info@gmail.com>
2022-09-07 11:22:27 -0300

    Update main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+import datetime
+
+from flask import Flask, render_template, request, redirect, url_for
 from utils.database import *
 
 app = Flask(__name__)
@@ -10,7 +12,7 @@ c = Curriculo(
     email='juciandecezare@hotmail.com',
     resumo='Hello, my name is Jucian',
     ultimaatualizacao='Today',
-    formacao=True,
+    formacao=None,
     atuacao=Atuacao(
         id=1,
         area='Ciencia da computaria',
@@ -19,27 +21,24 @@ c = Curriculo(
     ),
     projetos=Projetos(
         id=1,
-        projetosandamento=[
-            Projeto(
-                nome='DDoS do pellas',
-                descricao='cansei da vida',
-                cargahoraria=1
-            )
-        ],
-        projetosterminados=[
-            Projeto(
-                nome='Dormir',
-                descricao='Estou com sono',
-                cargahoraria=20
-            )
-        ]
-    )
-)
+        projetosandamento=[Projeto(
+            nome='DDoS do pellas',
+            descricao='cansei da vida',
+            cargahoraria=1
+        )],
+        projetosterminados=[Projeto(
+            nome='Dormir',
+            descricao='Estou com sono',
+            cargahoraria=20
+        )]
+    ))
 
 
 @app.route('/')
 def main():
-    return render_template('Curriculo.html', title='WELCOME', curriculo=c)
+    username = mysql_select('users', ['username'], filtros={'ip': request.environ['REMOTE_ADDR']})
+    if username: username = username[0][0]
+    return render_template('Curriculo.html', title='WELCOME', curriculo=c, username=username)
 
 
 @app.route('/adicionarcurriculo', methods=['GET', 'POST'])
@@ -72,11 +71,9 @@ def AddCurriculo():
                 certificado='??' #DESCOBRIR O Q CARALHOS FZR AQ
             )
 
-            mysql_insert(
-                'cursos', {
-                    'id': 'DEFAULT',
-                }
-            )
+            mysql_insert('cursos', {
+                'id': 'DEFAULT',
+            })
 
         else:
             nome = request.form.get('nome')
@@ -97,6 +94,58 @@ def AddCurriculo():
             empregos_passados = ['n sei']
 
             return f"{formacao}"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def Login():
+    if request.method == 'GET':
+        return render_template('login.html', title='Login')
+    elif request.method == 'POST':
+        user = {
+            'username': request.form.get('username'),
+            'password': request.form.get('password'),
+            'ip': request.environ['REMOTE_ADDR']
+        }
+        user_db = mysql_select('users', filtros={'username': user['username'], 'password': user['password']})
+        if user_db:
+            mysql_command(f"UPDATE users SET ip = '{user['ip']}' WHERE username = '{user['username']}' limit 1;")
+        else:
+            return f'Usuário ou senha incorretos'
+
+        return redirect(url_for('main'))
+
+
+@app.route('/logout')
+def Logout():
+    mysql_command(f"UPDATE users SET ip = Null WHERE ip = '{request.environ['REMOTE_ADDR']}' limit 1;")
+    return redirect(url_for('main'))
+
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def Register():
+    if request.method == 'GET':
+        return render_template('Cadastro.html', title='Cadastro')
+    elif request.method == 'POST':
+        if request.form.get('password') != request.form.get('password_confirm'):
+            return "SENHAS NÃO COINCIDEM"
+        if mysql_select('users', filtros={'username': request.form.get('username')}):
+            return "Esse nome de usuário já está registrado."
+        if mysql_select('users', filtros={'email': request.form.get('email')}):
+            return "Esse email está logado."
+
+        user = {
+            'username': request.form.get('username'),
+            'email': request.form.get('email'),
+            'password': request.form.get('password'),
+            'createdate': f"{datetime.datetime.today().year}-{datetime.datetime.today().month:0>2}-{datetime.datetime.today().day:0>2}"
+        }
+
+        try:
+            mysql_insert('users', user)
+            return redirect(url_for('Login'))
+        except:
+            return 'Um erro inesperado ocorreu durante a utilização do banco de dados.'
 
 
 if __name__ == '__main__':
---
+++ templates/script.js
@@ -0,0 +1,106 @@
+/* EMAIL
+* <input id='a' type="email" name="email">
+* <button id="bt" onclick="alert(validate('a'));">Ver</button>
+*/
+
+/* CPF
+* <input id="abc" type="cpf" name="{{ type }}" maxlength="14" onkeyup="formatcpf('[#] {{ id }}')">
+* <button onclick="alert(validate(' {{ input[id] }} '));">text</button>
+*/
+
+function formatcpf(tag) {
+	let stag;
+	if(tag.charAt(0) != ('#') && tag.charAt(0) != ('.')) {
+		try{
+			stag = document.querySelector(`#${tag}`);
+		}
+		catch{
+			stag = document.querySelector(`.${tag}`);
+		}
+	}
+	else{
+		stag = document.querySelector(tag);
+	}
+	if(stag.value.length == 3 || stag.value.length == 7) {
+		stag.value += '.';
+	}
+	else if(stag.value.length == 11) {
+		stag.value += '-';
+	}
+}
+
+function validate(tag) {
+	let stag;
+	let tp;
+	let val;
+	if(tag.charAt(0) != ('#') && tag.charAt(0) != ('.')) {
+		try{
+			stag = document.querySelector(`#${tag}`);
+		}
+		catch{
+			stag = document.querySelector(`.${tag}`);
+		}
+	}
+	else{
+		stag = document.querySelector(tag);
+	}
+	val = stag.value;
+	tp = stag.name.toLowerCase();
+	switch(tp) {
+		case "email":
+			return(stag.checkValidity());
+			break;
+		case "cpf":
+			val = val.replace(/[^\d]+/g,'');
+			let sum = 0;
+			let rest = 0;
+			for(let n = 0; n <= 9; n++){
+				let re = new RegExp(`^${n}+$`);
+				if(re.test(val)){
+					return(false);
+				}
+			}
+			for(i = 1; i <= 9; i++){
+				sum += parseInt(val.substring(i-1,i)) * (11 - i);
+			}
+			rest = (sum * 10) % 11;
+			if((rest == 10) || (rest == 11)) {
+				rest = 0;
+			}
+			if(rest != parseInt(val.substring(9,10))) {
+				return(false);
+			}
+			sum = 0;
+			for(i = 1; i <= 10; i++) {
+				sum += parseInt(val.substring(i-1,i)) * (12 - i);
+			}
+			rest = (sum * 10) % 11;
+			if((rest == 10) || (rest == 11)) {
+				rest = 0;
+			}
+			if(rest != parseInt(val.substring(10,11))){
+				return(false);
+			}
+			return(true);
+			break;
+	}
+}
+
+function setCookie(key,value){
+	document.cookie = `${key}=${value}`;
+}
+
+function readCookie(key='', convert=false) {
+	let result;
+	let dc = document.cookie
+	  .split(';')
+	  .map(cookie => cookie.split('='))
+	  .reduce((acumulator,[k,v]) => ({...acumulator,[k.trim()]: decodeURIComponent(v) }),{});
+	key == '' ? result = dc : result = dc[key];
+	!isNaN(result) && convert ? result = Number(result) : {};
+	return(result);
+}
+
+function deleteCookie(key) {
+	document.cookie = `${key}=; expires = Thu, 01 Jan 1970 00:00:01 GTM;`;
+}
